9f57cb6720ec6b4e2b140354b0a45eedb1b87298
Ardiant Utomo <ardiantutomo@gmail.com>
2023-06-14 20:26:26 +0700

    implement user search

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import { useState } from "react";
 import "./App.css";
 import SearchForm from "./components/SearchForm";
+import { Users, UsersResponse } from "./interface/user.interface";
 
 function App() {
   const [query, setQuery] = useState<string>("");
+  const [userData, setUserData] = useState<Users[] | undefined>();
 
-  const search = async () => {};
+  const search = async () => {
+    fetch(`https://api.github.com/search/users?q=${query}&per_page=5`)
+      .then((response: Response) => {
+        return response.json();
+      })
+      .then((data: UsersResponse) => {
+        setUserData(data.items);
+      });
+  };
 
-  return <SearchForm query={query} setQuery={setQuery} search={search} />;
+  return (
+    <div>
+      <SearchForm query={query} setQuery={setQuery} search={search} />;
+      <p>Showing user for "{query}"</p>
+    </div>
+  );
 }
 
 export default App;
